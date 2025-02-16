52ccb57c82bb9610abc5ccaa51bf1e78a15b5369
Rosse Mathys <rosse.mathys28@gmail.com>
2025-02-16 19:19:11 +0100

    final changes for ue

--- init_db.py
+++ init_db.py
@@ -1,65 +1,63 @@
-# pylint: disable=missing-module-docstring
 import io
-
-import duckdb
 import pandas as pd
-import streamlit as st
+import duckdb
 
-con = duckdb.connect(database='data/exercices_sql_tables.duckdb', read_only=False)
+con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
 
-# Exercises list
+# ------------------------------------------------------------
+# EXERCISES LIST
+# ------------------------------------------------------------
 
 data = {
     "theme": ["cross_joins", "cross_joins"],
-    "exercise_name": ["beverages_and_foods", "sizes_and_trademarks"],
+    "exercise_name": ["beverages_and_food", "sizes_and_trademarks"],
     "tables": [["beverages", "food_items"], ["sizes", "trademarks"]],
-    "last_reviewed": ["1980-01-01", "1970-01-01"]
+    "last_reviewed": ["1980-01-01", "1970-01-01"],
 }
-
-
-memory_state_df=pd.DataFrame(data)
-# con.execute("CREATE TABLE IF NOT EXISTS memory_state AS SELECT * FROM memory_state_df")
+memory_state_df = pd.DataFrame(data)
 con.execute("CREATE OR REPLACE TABLE memory_state AS SELECT * FROM memory_state_df")
 
-CSV = """
+
+# ------------------------------------------------------------
+# CROSS JOIN EXERCISES
+# ------------------------------------------------------------
+csv = """
 beverage,price
-orange juice, 2.5
-expresso, 2
-tea, 3"""
+orange juice,2.5
+Expresso,2
+Tea,3
+"""
+beverages = pd.read_csv(io.StringIO(csv))
+con.execute("CREATE OR REPLACE TABLE beverages AS SELECT * FROM beverages")
 
-beverages = pd.read_csv(io.StringIO(CSV))
-con.execute('CREATE TABLE IF NOT EXISTS beverages AS SELECT * FROM beverages')
+csv2 = """
+food_item,food_price
+cookie juice,2.5
+chocolatine,2
+muffin,3
+"""
+food_items = pd.read_csv(io.StringIO(csv2))
+con.execute("CREATE OR REPLACE TABLE food_items AS SELECT * FROM food_items")
 
 
-CSV2 = """
-food_item, food_price
-cookie juice, 2.5
-chocolatine, 2
-muffin, 3"""
-
-food_items = pd.read_csv(io.StringIO(CSV2))
-con.execute('CREATE TABLE IF NOT EXISTS food_items AS SELECT * FROM food_items')
-
-CSV3 = """
+sizes = """
 size
 XS
 M
 L
 XL
 """
+sizes = pd.read_csv(io.StringIO(sizes))
+con.execute("CREATE OR REPLACE TABLE sizes AS SELECT * FROM sizes")
 
-sizes = pd.read_csv(io.StringIO(CSV3))
-con.execute('CREATE TABLE IF NOT EXISTS sizes AS SELECT * FROM sizes')
-
-CSV4 = """
+trademarks = """
 trademark
-NIKE
+Nike
 Asphalte
 Abercrombie
 Lewis
 """
-
-trademarks= pd.read_csv(io.StringIO(CSV4))
-con.execute('CREATE TABLE IF NOT EXISTS trademarks AS SELECT * FROM trademarks')
+trademarks = pd.read_csv(io.StringIO(trademarks))
+con.execute("CREATE OR REPLACE TABLE trademarks AS SELECT * FROM trademarks")
 
 con.close()
